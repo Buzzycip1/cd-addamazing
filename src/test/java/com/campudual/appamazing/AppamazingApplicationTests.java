ec3ce96548c83012733a0e36d462bf51e3fe6b03
Henry Sampedro <hsampedro@gmail.com>
2023-09-25 09:03:37 +0200

    Add templateDB script and txt,  add plugins to pom.xml

---
+++ src/test/java/com/campudual/appamazing/AppamazingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campudual.appamazing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppamazingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
