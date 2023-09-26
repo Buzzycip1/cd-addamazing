2b2e3fe5f530ab84c403b3fb0717f1d938d68da5
Henry Sampedro <hsampedro@gmail.com>
2023-09-26 13:47:20 +0200

    Add CONTATCS table to DB

---
+++ src/main/java/com/campudual/appamazing/model/dto/ContactDto.java
@@ -0,0 +1,58 @@
+package com.campudual.appamazing.model.dto;
+
+public class ContactDto {
+    private int id;
+    private String name;
+    private String  surname;
+    private String surname2;
+    private String email;
+    private String phone_number;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getSurname2() {
+        return surname2;
+    }
+
+    public void setSurname2(String surname2) {
+        this.surname2 = surname2;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+}
