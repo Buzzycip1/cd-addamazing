6ddf8e7c1c862de2da06f47b583dc0bfd4a1bde5
Henry Sampedro <hsampedro@gmail.com>
2023-09-26 14:31:57 +0200

    Add CONTATCS table to DB

---
+++ src/main/java/com/campudual/appamazing/model/dto/dtomapper/ContactMapper.java
@@ -0,0 +1,19 @@
+package com.campudual.appamazing.model.dto.dtomapper;
+
+import com.campudual.appamazing.model.Contact;
+import com.campudual.appamazing.model.dto.ContactDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+@Mapper
+public interface ContactMapper {
+
+
+    ContactMapper INSTANCE = Mappers.getMapper(ContactMapper.class);
+
+    ContactDto toDTO(Contact contact);
+    List<ContactDto> toDTOList(List<Contact> contacts);
+    Contact toEntity(ContactDto contactDto);
+
+}
