678206c8bdf95db26af4c594d6681b3d3b3862a8
Henry Sampedro <hsampedro@gmail.com>
2023-09-27 08:16:06 +0200

    Add CONTATCS table to DB

--- src/main/java/com/campudual/appamazing/model/dao/ContactDao.java
+++ src/main/java/com/campudual/appamazing/model/dao/ContactDao.java
@@ -3,6 +3,9 @@ package com.campudual.appamazing.model.dao;
 
 import com.campudual.appamazing.model.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+// DAO es la capa que permite el  acceso entre el modelo creado y las bases de datos
+
 public interface ContactDao extends JpaRepository< Contact, Integer> {
 
 
