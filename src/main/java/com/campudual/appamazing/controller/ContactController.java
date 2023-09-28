9c023e300ffd3b7bd8e1109b8b2d89b5aa718d2b
Henry Sampedro <hsampedro@gmail.com>
2023-09-28 20:58:26 +0200

    1. Implemented Get and GetAll Methods for Contact Controller 2. Refactored both Mappers to one single package 3. Reinstalled the BD cause of mapping file swap 4. Implemented Post from ContactController

--- src/main/java/com/campudual/appamazing/controller/ContactController.java
+++ src/main/java/com/campudual/appamazing/controller/ContactController.java
@@ -2,10 +2,12 @@ package com.campudual.appamazing.controller;
 
 
 import com.campudual.appamazing.api.IContactService;
+import com.campudual.appamazing.model.dto.ContactDto;
+import com.campudual.appamazing.model.dto.ProductDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/contacts")
@@ -18,6 +20,29 @@ public class ContactController {
     public String testController() {
         return "Product controller works!";
     }
+    @PostMapping
+    public String testController(@RequestBody String name) {
+        return "Product controller works, " + name + "!";
+    }
+
+    @GetMapping(value = "/testMethod")
+    public String testControllerMethod() {
+        return "Product controller method works!";
+    }
+
+    @PostMapping(value = "/get")
+    public ContactDto queryContact(@RequestBody ContactDto contact) {
+        return this.contactService.queryContact(contact);
+    }
+    @GetMapping(value = "/getAll")
+    public List<ContactDto> queryAllContact() {
+        return this.contactService.queryAllContact();
+    }
+    @PostMapping(value = "/add")
+    public int insertContact(@RequestBody ContactDto contact) {
+        return this.contactService.insertContact(contact);
+    }
+
 
 
 
