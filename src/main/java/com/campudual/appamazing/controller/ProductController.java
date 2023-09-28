5d2759b0b03946cccf730e82a60dcfadb3b31f14
Henry Sampedro <hsampedro@gmail.com>
2023-09-28 09:07:53 +0200

    Add CONTATCS table to DB

---
+++ src/main/java/com/campudual/appamazing/controller/ProductController.java
@@ -0,0 +1,19 @@
+package com.campudual.appamazing.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+
+public class ProductController {
+
+        @GetMapping
+        public String testController(){
+            return "Product controller works!";
+        }
+
+
+}
